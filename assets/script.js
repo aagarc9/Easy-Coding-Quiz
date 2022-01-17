06b54df849673e944d06be7c30e2231d89f8724e
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-16 19:24:26 -0800

    Updates to Score

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 let questionInfo = document.getElementById('question')
-let choiceA = document.getElementById('A')
-let choiceB = document.getElementById('B')
-let choiceC = document.getElementById('C')
-let choiceD = document.getElementById('D')
+let choice1 = document.getElementById('1')
+let choice2 = document.getElementById('2')
+let choice3 = document.getElementById('3')
+let choice4 = document.getElementById('4')
 let progress = document.getElementById('progress')
 let clockTimer = document.getElementById ('timer')
 let scoreCounter = document.getElementById('score')
+let choicesEl = document.getElementsByClassName('.choice')
 
 let score = 0;
 let time = 60;
@@ -13,43 +14,43 @@ let time = 60;
 let questions = [
     {
         question: "_______ is the process of finding errors and fixing them within a program.",
-        answer: "Debugging",
-        choiceA: "Compiling",
-        choiceB:"Executing",
-        choiceC:"Debugging",
-        choiceD:"Scanning",
+        answer: "3",
+        choice1: "Compiling",
+        choice2:"Executing",
+        choice3:"Debugging",
+        choice4:"Scanning",
     },
     {
         question: "What does HTML stand for?",
-        answer: "Hyperlinks and Text Markup Language",
-        choiceA: "Hyperlinks and Text Markup Language",
-        choiceB:"Home Tool Markup Language",
-        choiceC:"Hyper Text Markup Language",
-        choiceD:"Hyper Text Markdown Language",
+        answer: "3",
+        choice1: "Hyperlinks and Text Markup Language",
+        choice2:"Home Tool Markup Language",
+        choice3:"Hyper Text Markup Language",
+        choice4:"Hyper Text Markdown Language",
     },
     {
         question: "What is the correct sequence of HTML tags for starting a webpage?",
-        answer: "HTML, Head, Title",
-        choiceA:"Title, Head, HTML" ,
-        choiceB:"HTML, Head, Title",
-        choiceC:"Head, Title, HTML",
-        choiceD:"Body, HTML, Title",
+        answer: "2",
+        choice1:"Title, Head, HTML" ,
+        choice2:"HTML, Head, Title",
+        choice3:"Head, Title, HTML",
+        choice4:"Body, HTML, Title",
     },
     {
         question: "Which of the following defines a measurement in screen pixels?",
-        answer: "px",
-        choiceA:"px",
-        choiceB:"vh",
-        choiceC:"vw",
-        choiceD:"vmin",
+        answer: "1",
+        choice1:"px",
+        choice2:"vh",
+        choice3:"vw",
+        choice4:"vmin",
     },
     {
         question: " What is the correct HTML tag for inserting a line break?",
-        answer: "Br",
-        choiceA:"Break",
-        choiceB:"Strong",
-        choiceC:"Lb",
-        choiceD:"Br",
+        answer: "4",
+        choice1:"Break",
+        choice2:"Strong",
+        choice3:"Lb",
+        choice4:"Br",
     },
 ];
 
@@ -63,20 +64,18 @@ let questionIndex = 0;
 // start Game
 function startGame () {
     // timer
-    getNewQuestion()
-    progressBar()
+    // getNewQuestion()
+    // questionTimer()
 }
 
 // create a function to display in HTML
 function getNewQuestion () {
     let q =questions[questionIndex];
     questionInfo.innerHTML = q.question;
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-
-    // console.log(q.answer)
+    choice1.innerHTML = q.choice1;
+    choice2.innerHTML = q.choice2;
+    choice3.innerHTML = q.choice3;
+    choice4.innerHTML = q.choice4;
 }
 // test code to see if next question pops up
 getNewQuestion()
@@ -84,43 +83,39 @@ questionTimer()
 
 
 // create a function the check answer
-function checkAnswer() {
+function checkAnswer(correct) {
     let q =questions[questionIndex];
-    if(q.answer){
+    if(q.answer === correct){
         score++;
-        // answeringquestion();
-        // questionTimer();
+        console.log(score);
     }else {
-        questionTimer();
-        // answeringquestion();
     }
     if(questionIndex < lastQuestionIndex) {
         questionIndex++;
         getNewQuestion()
     } else {
-        // endQuiz()
+        clearInterval();
+       endQuiz()
     }
 }
-
-function questionTimer(correct) {
-    // check if the user guessed incorrectly
+function questionTimer() {
+    // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    time--;
+// check if the user guessed incorrectly
     let q =questions[questionIndex]; 
-    if(q.answer !== correct){
+    if(q.answer !== true){
         // time has to be subtracted 
         time -= 15;
-        clockTimer.innerText = time
-        if(time<0) {
-            time =0;
+        clockTimer.innerText = time;
+        if (time === 0) {
+            clearInterval(timerInterval);
+          }
         }
-    }
-    // questionCounter++;
-    // if(questionCounter === availableQuestion.length) {
-    //     endQuiz() // or could be showResults()
-    // } else {
-    //     getNewQuestion(); // okay just continue 
-    // }
+    }, 1000);
+
 }
-   console.log(time)
+
 
 
 // session storage, cookies, cache, local storage 
@@ -141,26 +136,14 @@ function savedScores() {
 
 }
 
-function progressBar() {
-    for(let i = 0; i <= lastQuestionIndex; i++){
-        progress.innerHTML += "<div class='prog' id="+ i +"></div>";
-    } 
-}
-function answeringquestion() {
-    document.getElementById(questionIndex).style.backgroundColor ="purple"
-}
 
 function endQuiz() {
     scoreCounter.style.display = "block";
     let scorePercent = Math.round(100 * score / questions.length);
-    let display = (scorePercent >= 80) ? "80%":
-    (scorePercent >= 60) ? "60%":
-    (scorePercent >= 40) ? "40%":
-    (scorePercent >= 20) ? "20%":
-    (scorePercent >= 0) ? "0%":
+    let display = scorePercent
+  
 
     scoreCounter.innerHTML = display;
 }
 
 startGame()
-console.log(display)
